884138fc3fd618990d40fdcc10397c3f20c6377b
cabb99 <cabb99@gmail.com>
2016-04-09 10:03:22 -0500

    Adds conditional

--- Multi_execute.py
+++ Multi_execute.py
@@ -9,6 +9,7 @@ if __name__=='__main__':
     parser=argparse.ArgumentParser(description='Executes a command multiple times, by entering in to the directory')
     parser.add_argument('Folders',help='The folder where the commands will be executed')
     parser.add_argument('-c','--command',help='The command you want to execute')
+    parser.add_argument('-i','--condition', nargs=2, metavar='Cond',help='File_to_read line_to_find If it finds a line in the file, it will be executed')
     args=parser.parse_args()
     
     #Remember original directory
@@ -16,7 +17,14 @@ if __name__=='__main__':
     for folder in glob.glob(args.Folders):
         #Enter the directory
         os.chdir(folder)
+        #Test condition
+        Execute=True        
+        with open(max(glob.iglob(args.condition[0]), key=os.path.getctime)) as handle:
+            for line in handle:
+                if args.condition[1] in line:
+                    Execute=False        
         #Execute the command
-        subprocess.call(args.command, shell=True)
+        if Execute:        
+            subprocess.call(args.command, shell=True)
         #Back to the original directory
         os.chdir(initial_dir)
